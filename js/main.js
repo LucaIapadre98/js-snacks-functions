03a6d02daf2927392f878e626cd197d38de712a8
LucaIapadre98 <luca.iap.1998@gmail.com>
2025-04-08 17:45:24 +0200

    imposto la struttura

---
+++ js/main.js
@@ -0,0 +1,21 @@
+/* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito
+ (es. "Ciao [nome]") */
+
+const userName = 'Mario';
+const greet = prompt("Fai un saluto:");
+console.log(greet);
+
+// Dichiara la funzione qui.
+function greetUserName(greet && Name) {
+    alert(userName);
+    return true;
+}
+
+// Invoca la funzione qui e stampa il risultato in console
+
+greetUserNameserName();
+console.log(greetUserName);
+
+    
+
+//Risultato atteso se si passa 'Mario': // ciao Mario
